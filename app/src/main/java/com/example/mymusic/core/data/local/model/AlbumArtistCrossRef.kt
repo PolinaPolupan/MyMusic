7a900bee61db95711a0d81f23126d478dac0c212
PolinaPolupan <polupanpolina9@gmail.com>
2024-06-28 17:35:33 +0300

    fix: Foreign keys to cross references are added
    Foreign keys were added for propagating updates to the child keys.
    onDelete is set to NO_ACTION, because there can be other dependencies
    (like playlists, singles, etc.)

--- app/src/main/java/com/example/mymusic/core/data/local/model/AlbumArtistCrossRef.kt
+++ app/src/main/java/com/example/mymusic/core/data/local/model/AlbumArtistCrossRef.kt
@@ -1,13 +1,27 @@
 package com.example.mymusic.core.data.local.model
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.Index
 
 /**
- * Cross reference for many to many relationship between [LocalSimplifiedArtist] and [LocalAlbum]
+ * Cross reference for many-to-many relationship between [LocalSimplifiedArtist] and [LocalAlbum].
+ * Foreign keys were added for propagating updates to the child keys. onDelete is set to NO_ACTION
+ * because there can be other dependencies (like playlists, singles, etc.)
  */
 @Entity(
     primaryKeys = ["simplifiedArtistId", "albumId"],
+    foreignKeys = [
+        ForeignKey(
+            entity = LocalSimplifiedArtist::class,
+            parentColumns = arrayOf("simplifiedArtistId"),
+            childColumns = arrayOf("simplifiedArtistId"),
+            onUpdate = ForeignKey.CASCADE),
+        ForeignKey(
+            entity = LocalAlbum::class,
+            parentColumns = arrayOf("albumId"),
+            childColumns = arrayOf("albumId"),
+            onUpdate = ForeignKey.CASCADE)],
     indices = [
         Index(value = ["simplifiedArtistId"]),
         Index(value = ["albumId"])
