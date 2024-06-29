864495cfb0aed960c6ff2ddd8f8802ec31a96317
PolinaPolupan <polupanpolina9@gmail.com>
2024-06-29 13:19:07 +0300

    feat: View model loads data from the API before app ui loading

--- app/src/main/java/com/example/mymusic/MainActivity.kt
+++ app/src/main/java/com/example/mymusic/MainActivity.kt
@@ -14,24 +14,46 @@ import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import com.example.mymusic.core.data.Constants
 import com.example.mymusic.core.designSystem.theme.MyMusicTheme
 import com.spotify.android.appremote.api.ConnectionParams
 import com.spotify.android.appremote.api.Connector
 import com.spotify.android.appremote.api.SpotifyAppRemote
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    val viewModel: MainActivityViewModel by viewModels()
+    private val viewModel: MainActivityViewModel by viewModels()
 
     var spotifyAppRemote: SpotifyAppRemote? = null
 
     @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+
+        val isLoading = mutableStateOf(true)
+
+        // Load data from the network and refresh database
+        lifecycleScope.launch {
+            // Initialize auth state (access token)
+            viewModel.restoreState()
+            viewModel.refresh()
+            isLoading.value = false
+        }
+
+        // Keep the splash screen on-screen until the UI state is loaded. This condition is
+        // evaluated each time the app needs to be redrawn so it should be fast to avoid blocking
+        // the UI.
+        splashScreen.setKeepOnScreenCondition {
+            isLoading.value
+        }
 
         enableEdgeToEdge(
             // This app is only ever in dark mode, so hard code detectDarkMode to true.
@@ -41,7 +63,7 @@ class MainActivity : ComponentActivity() {
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         )
-        val authState = viewModel.authState
+
         setContent {
             MyMusicTheme(dynamicColor = true) {
                 // A surface container using the 'background' color from the theme
