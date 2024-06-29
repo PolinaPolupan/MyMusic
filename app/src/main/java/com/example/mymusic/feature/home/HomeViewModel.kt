337d2f9b2019d5ecb724b23880c46ef5a082a8ec
PolinaPolupan <polupanpolina9@gmail.com>
2024-06-29 13:17:11 +0300

    feat: View model loads recommendations from the Spotify API

--- app/src/main/java/com/example/mymusic/feature/home/HomeViewModel.kt
+++ app/src/main/java/com/example/mymusic/feature/home/HomeViewModel.kt
@@ -1,42 +1,41 @@
 package com.example.mymusic.feature.home
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.mymusic.core.data.AuthorizationManager
 import com.example.mymusic.core.data.MusicRepository
 import com.example.mymusic.core.data.UserDataRepository
-import com.example.mymusic.core.data.network.MyMusicAPIService
 import com.example.mymusic.model.Artist
 import com.example.mymusic.model.Track
 import com.example.mymusic.core.ui.PreviewParameterData
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.drop
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.launch
-import net.openid.appauth.AuthState
-import org.json.JSONObject
-import retrofit2.Response
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     userDataRepository: UserDataRepository,
-    private val musicRepository: MusicRepository
+    musicRepository: MusicRepository
 ): ViewModel()
 {
     private val _userDataFlow = userDataRepository.userPreferencesFlow
+    private val _recommendationsFlow = musicRepository.getRecommendationsStream()
 
     val uiState: StateFlow<HomeUiState> =
-        _userDataFlow.map {
-            HomeUiState.Success(it.imageUrl)
+        combine(
+           _userDataFlow,
+            _recommendationsFlow
+        ) {
+            userData, recommendations ->
+            when (recommendations.isEmpty() || userData.authState.isNullOrBlank()) {
+                true -> HomeUiState.Loading
+                false ->  HomeUiState.Success(
+                    userImageUrl = userData.imageUrl,
+                    topPicks = recommendations
+                )
+            }
         }
             .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000L), HomeUiState.Loading)
 }
@@ -45,7 +44,7 @@ sealed interface HomeUiState {
     data object Loading: HomeUiState
     data class Success(
         val userImageUrl: String?,
-        val topPicks: List<Track> = PreviewParameterData.tracks,
+        val topPicks: List<Track>,
         val moreLikeArtists: Map<Artist, List<Track>> = PreviewParameterData.moreLikeArtists,
         val recentlyPlayed: List<Track> = PreviewParameterData.tracks
     ): HomeUiState
