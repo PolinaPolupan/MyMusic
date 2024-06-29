d11841cda4855924a5161929b263d011162d0365
PolinaPolupan <polupanpolina9@gmail.com>
2024-06-29 13:21:31 +0300

    feat: Refresh function is added

--- app/src/main/java/com/example/mymusic/MainActivityViewModel.kt
+++ app/src/main/java/com/example/mymusic/MainActivityViewModel.kt
@@ -2,34 +2,37 @@ package com.example.mymusic
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.mymusic.core.data.AuthorizationManager
+import com.example.mymusic.core.data.MusicRepository
 import com.example.mymusic.core.data.UserDataRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainActivityViewModel @Inject constructor(
     private val authorizationManager: AuthorizationManager,
-    userDataRepository: UserDataRepository
+    userDataRepository: UserDataRepository,
+    private val musicRepository: MusicRepository
 ): ViewModel() {
 
-    val authState: Flow<String?> =
+    private val authState: Flow<String?> =
         userDataRepository.userPreferencesFlow
             .filterNotNull()
             .map {
                 it.authState
             }
 
-    init {
-        viewModelScope.launch {
-            Log.i("MainActivity", authState.toString())
-            authorizationManager.restoreState(authState.first())
-        }
+    suspend fun restoreState() {
+        Log.i("MainActivity", authState.toString())
+        authorizationManager.restoreState(authState.first())
+
+    }
+
+    suspend fun refresh() {
+        musicRepository.refresh()
     }
 }
