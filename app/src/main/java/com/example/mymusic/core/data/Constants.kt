e55a434d6fe17da3d36b87b101dbd9bd00b137cc
PolinaPolupan <polupanpolina9@gmail.com>
2024-06-30 14:07:28 +0300

    feat: Add recently played read scope, make values const

--- app/src/main/java/com/example/mymusic/core/data/Constants.kt
+++ app/src/main/java/com/example/mymusic/core/data/Constants.kt
@@ -2,32 +2,32 @@ package com.example.mymusic.core.data
 
 class Constants {
     companion object {
-        val DATASTORE_PREFERENCES_NAME = "AUTH_STATE_PREFERENCE"
-        val AUTH_STATE = "AUTH_STATE"
+        const val DATASTORE_PREFERENCES_NAME = "AUTH_STATE_PREFERENCE"
+        const val AUTH_STATE = "AUTH_STATE"
 
-        val SCOPE_EMAIL = "user-read-email"
-        val SCOPE_UGC_IMAGE_UPLOAD = "ugc-image-upload"
-        val SCOPE_USER_READ_PLAYBACK_STATE = "user-read-playback-state"
-        val SCOPE_USER_MODIFY_PLAYBACK_STATE = "user-modify-playback-state"
-        val SCOPE_USER_READ_CURRENTLY_PLAYING = "user-read-currently-playing"
-        val SCOPE_APP_REMOTE_CONTROL = "app-remote-control"
-        val SCOPE_STREAMING = "streaming"
-        val SCOPE_PLAYLIST_READ_PRIVATE = "playlist-read-private"
-        val SCOPE_PLAYLIST_MODIFY_PRIVATE = "playlist-modify-private"
-        val SCOPE_PLAYLIST_MODIFY_PUBLIC = "playlist-modify-public"
-        val SCOPE_USER_READ_PRIVATE = "user-read-private"
+        const val SCOPE_EMAIL = "user-read-email"
+        const val SCOPE_UGC_IMAGE_UPLOAD = "ugc-image-upload"
+        const val SCOPE_USER_READ_PLAYBACK_STATE = "user-read-playback-state"
+        const val SCOPE_USER_MODIFY_PLAYBACK_STATE = "user-modify-playback-state"
+        const val SCOPE_USER_READ_CURRENTLY_PLAYING = "user-read-currently-playing"
+        const val SCOPE_APP_REMOTE_CONTROL = "app-remote-control"
+        const val SCOPE_STREAMING = "streaming"
+        const val SCOPE_PLAYLIST_READ_PRIVATE = "playlist-read-private"
+        const val SCOPE_PLAYLIST_MODIFY_PRIVATE = "playlist-modify-private"
+        const val SCOPE_PLAYLIST_MODIFY_PUBLIC = "playlist-modify-public"
+        const val SCOPE_USER_READ_PRIVATE = "user-read-private"
+        const val SCOPE_USER_READ_RECENTLY_PLAYED = "user-read-recently-played"
 
-        val IMAGE_URL = "picture"
-        val DATA_DISPLAY_NAME = "display_name"
-        val DATA_EMAIL = "email"
+        const val IMAGE_URL = "picture"
+        const val DATA_DISPLAY_NAME = "display_name"
+        const val DATA_EMAIL = "email"
 
-        val CLIENT_ID = "964f3c103fd64e478848b059f9240d73"
-        val CODE_VERIFIER_CHALLENGE_METHOD = "S256"
-        val MESSAGE_DIGEST_ALGORITHM = "SHA-256"
+        const val CLIENT_ID = "964f3c103fd64e478848b059f9240d73"
+        const val CODE_VERIFIER_CHALLENGE_METHOD = "S256"
+        const val MESSAGE_DIGEST_ALGORITHM = "SHA-256"
 
-        val URL_AUTHORIZATION = "https://accounts.spotify.com/authorize"
-        val URL_TOKEN_EXCHANGE = "https://accounts.spotify.com/api/token"
-        val URL_AUTH_REDIRECT = "com.example.mymusic://callback"
-
+        const val URL_AUTHORIZATION = "https://accounts.spotify.com/authorize"
+        const val URL_TOKEN_EXCHANGE = "https://accounts.spotify.com/api/token"
+        const val URL_AUTH_REDIRECT = "com.example.mymusic://callback"
     }
 }
