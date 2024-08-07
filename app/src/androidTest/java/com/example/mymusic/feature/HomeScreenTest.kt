d31cf5cf7ff0372d9622a9145e1a0e5f11fd5217
PolinaPolupan <polupanpolina9@gmail.com>
2024-08-07 19:49:26 +0300

    test: Add home screen test

--- app/src/androidTest/java/com/example/mymusic/feature/HomeScreenTest.kt
+++ app/src/androidTest/java/com/example/mymusic/feature/HomeScreenTest.kt
@@ -1,37 +1,50 @@
 package com.example.mymusic.feature
 
+import androidx.activity.ComponentActivity
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.paging.PagingData
+import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.filters.MediumTest
-import com.example.mymusic.MainActivity
-import com.example.mymusic.core.data.repository.MusicRepository
 import com.example.mymusic.core.designSystem.theme.MyMusicTheme
-import com.example.mymusic.feature.home.HomeScreen
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import com.example.mymusic.feature.home.HomeContent
+import com.example.mymusic.feature.home.HomeUiState
+import com.example.mymusic.model.Track
+import kotlinx.coroutines.flow.flowOf
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import javax.inject.Inject
-
 
 @RunWith(AndroidJUnit4::class)
 class HomeScreenTest {
 
-
     @get:Rule
-    val composeTestRule = createComposeRule()
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
-    @Inject
-    lateinit var repository: MusicRepository
-
-    @Before
-    fun setup() {
+    @Test
+    fun placeholders_whenScreenIsLoading_exist() {
         composeTestRule.setContent {
             MyMusicTheme {
-                HomeScreen(onTrackClick = {}, onNavigateToLogin = {})
+                HomeContent(
+                    uiState = HomeUiState.Loading,
+                    userImageUrl = "",
+                    onTrackClick = {},
+                    recentlyPlayed = flowOf(PagingData.from(emptyList<Track>())).collectAsLazyPagingItems()
+                )
             }
         }
+
+        composeTestRule
+            .onNodeWithTag("home:topPicksLoading")
+            .assertExists()
+
+        composeTestRule
+            .onNodeWithTag("home:recentlyPlayedLoading")
+            .assertExists()
+
+        composeTestRule
+            .onNodeWithTag("home:blurredImageHeaderLoading")
+            .assertExists()
     }
 }
