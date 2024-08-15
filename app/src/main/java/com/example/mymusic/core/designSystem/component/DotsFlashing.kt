5c0adfc0b945221591db3793e954c89f35548b33
PolinaPolupan <polupanpolina9@gmail.com>
2024-08-15 17:25:40 +0300

    feat: Flashing dots composable improvements
    Amount of dots can be set. Size of dots was removed from the function
    parameters

--- app/src/main/java/com/example/mymusic/core/designSystem/component/DotsFlashing.kt
+++ app/src/main/java/com/example/mymusic/core/designSystem/component/DotsFlashing.kt
@@ -6,37 +6,33 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun DotsFlashing(
-    dotSize: Dp = 24.dp,
-    delayUnit: Int = 600,
+    count: Int = 3,
+    delayUnit: Int = 300,
     @FloatRange(from = 0.0, to = 1.0) minAlpha: Float = 0.3f,
     @FloatRange(from = 0.0, to = 1.0) maxAlpha: Float = 0.7f,
     color: Color = MaterialTheme.colorScheme.primary
 ) {
-
     @Composable
     fun Dot(
-        alpha: Float
+        alpha: Float,
+        modifier: Modifier = Modifier
     ) = Spacer(
-        Modifier
-            .size(dotSize)
+        modifier
             .alpha(alpha)
             .background(
                 color = color,
                 shape = CircleShape
             )
+            .aspectRatio(1f)
     )
 
     val infiniteTransition = rememberInfiniteTransition(label = "infiniteTransition")
@@ -55,33 +51,17 @@ fun DotsFlashing(
         ), label = ""
     )
 
-    val alpha1 by animateAlphaWithDelay(0)
-    val alpha2 by animateAlphaWithDelay(delayUnit)
-    val alpha3 by animateAlphaWithDelay(delayUnit * 2)
-
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Center
-    ) {
-        val spaceSize = 2.dp
-
-        Dot(alpha1)
-        Spacer(Modifier.width(spaceSize))
-        Dot(alpha2)
-        Spacer(Modifier.width(spaceSize))
-        Dot(alpha3)
+    Row {
+        for (i in 0..<count) {
+            val alpha by animateAlphaWithDelay(delayUnit * i)
+            Dot(alpha, modifier = Modifier.fillMaxWidth().weight(3f))
+            if (i != count - 1) Spacer(modifier = Modifier.fillMaxWidth().weight(1f))
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DotsPreview() = MaterialTheme {
-    Column(modifier = Modifier.padding(4.dp)) {
-
-        Text(
-            text = "Dots flashing",
-            style = MaterialTheme.typography.titleLarge
-        )
-        DotsFlashing()
-    }
+    DotsFlashing()
 }
