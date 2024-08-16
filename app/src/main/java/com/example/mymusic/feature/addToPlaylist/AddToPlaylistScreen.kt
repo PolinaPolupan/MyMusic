6033a3a9467c7cb239659dec5da46835bcb242aa
PolinaPolupan <polupanpolina9@gmail.com>
2024-08-16 14:47:02 +0300

    feat: Add to playlist screen modifications
    Add loading state. Fix animations

--- app/src/main/java/com/example/mymusic/feature/addToPlaylist/AddToPlaylistScreen.kt
+++ app/src/main/java/com/example/mymusic/feature/addToPlaylist/AddToPlaylistScreen.kt
@@ -9,10 +9,10 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -22,6 +22,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.windowInsetsTopHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -31,9 +33,11 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -41,6 +45,8 @@ import androidx.compose.ui.draw.BlurredEdgeTreatment
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -51,24 +57,25 @@ import androidx.paging.PagingData
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.example.mymusic.R
+import com.example.mymusic.core.designSystem.component.AnimationBox
 import com.example.mymusic.core.designSystem.component.BlurredImageHeader
+import com.example.mymusic.core.designSystem.component.MyMusicIcons
+import com.example.mymusic.core.designSystem.component.PlaylistCard
+import com.example.mymusic.core.designSystem.component.PreviewParameterData
+import com.example.mymusic.core.designSystem.component.RectangleRoundedCornerPlaceholder
 import com.example.mymusic.core.designSystem.component.Sort
 import com.example.mymusic.core.designSystem.component.SortBottomSheet
 import com.example.mymusic.core.designSystem.component.SortOption
 import com.example.mymusic.core.designSystem.component.linearGradientScrim
-import com.example.mymusic.core.designSystem.component.MyMusicIcons
+import com.example.mymusic.core.designSystem.theme.DominantColorState
 import com.example.mymusic.core.designSystem.theme.DynamicThemePrimaryColorsFromImage
 import com.example.mymusic.core.designSystem.theme.MyMusicTheme
 import com.example.mymusic.core.designSystem.theme.rememberDominantColorState
-import com.example.mymusic.core.designSystem.util.contrastAgainst
 import com.example.mymusic.core.designSystem.util.darker
 import com.example.mymusic.core.designSystem.util.lerpScrollOffset
-import com.example.mymusic.core.designSystem.util.rememberScrollState
-import com.example.mymusic.model.Track
-import com.example.mymusic.core.designSystem.component.PreviewParameterData
-import com.example.mymusic.feature.library.playlistsList
 import com.example.mymusic.model.SimplifiedPlaylist
 import kotlinx.coroutines.flow.flowOf
+import kotlin.math.abs
 
 @Composable
 fun AddToPlayListScreen(
@@ -79,20 +86,28 @@ fun AddToPlayListScreen(
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
     when (uiState) {
-        /*TODO: Add loading screen*/
-        AddToPlaylistUiState.Loading -> Unit
+        AddToPlaylistUiState.Loading -> {
+
+            AddToPlayListContent(
+                uiState = uiState,
+                playlists = flowOf(PagingData.from(emptyList<SimplifiedPlaylist>())).collectAsLazyPagingItems(),
+                onSortOptionChanged = viewModel::setSortOption,
+                currentSortOption = viewModel.currentSortOption.value,
+                onBackPress = onBackPress,
+                modifier = modifier.fillMaxSize()
+            )
+        }
         is AddToPlaylistUiState.Success -> {
 
             val playlists = viewModel.savedPlaylists.collectAsLazyPagingItems()
 
             AddToPlayListContent(
-                track = (uiState as AddToPlaylistUiState.Success).track,
+                uiState = uiState,
                 playlists = playlists,
                 onSortOptionChanged = viewModel::setSortOption,
                 currentSortOption = viewModel.currentSortOption.value,
                 onBackPress = onBackPress,
-                modifier = modifier
-                    .fillMaxSize()
+                modifier = modifier.fillMaxSize()
             )
         }
     }
@@ -101,7 +116,7 @@ fun AddToPlayListScreen(
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun AddToPlayListContent(
-    track: Track,
+    uiState: AddToPlaylistUiState,
     playlists: LazyPagingItems<SimplifiedPlaylist>,
     onBackPress: () -> Unit,
     onSortOptionChanged: (SortOption) -> Unit,
@@ -109,26 +124,24 @@ fun AddToPlayListContent(
     modifier: Modifier = Modifier
 ) {
     val lazyListState = rememberLazyListState()
-    val scrollState = rememberScrollState(state = lazyListState)
 
-    val surfaceColor = MaterialTheme.colorScheme.surface
-    val dominantColorState = rememberDominantColorState { color ->
-        // We want a color which has sufficient contrast against the surface color
-        color.contrastAgainst(surfaceColor) >= 3f
+    val firstVisibleItemScrollOffset by remember {
+        derivedStateOf { lazyListState.firstVisibleItemScrollOffset }
     }
 
+    val firstVisibleItemIndex by remember {
+        derivedStateOf { lazyListState.firstVisibleItemIndex }
+    }
+
+    val dominantColorState = rememberDominantColorState()
+
     val alpha: Float by animateFloatAsState(
-        if (scrollState.value >= 140) 1f else 0.0f,
-        animationSpec = tween(500, easing = LinearOutSlowInEasing
-        ), label = "album:textAlpha"
+        if (abs(firstVisibleItemScrollOffset) >= 140 || firstVisibleItemIndex > 0) 1f else 0.0f,
+        animationSpec = tween(500, easing = LinearOutSlowInEasing),
+        label = "addToPlaylist:alpha"
     )
 
     DynamicThemePrimaryColorsFromImage(dominantColorState) {
-        // When the selected image url changes, call updateColorsFromImageUrl() or reset()
-        LaunchedEffect(track.album.imageUrl) {
-            dominantColorState.updateColorsFromImageUrl(track.album.imageUrl)
-        }
-
         Box(
             modifier = modifier.linearGradientScrim(
                 color = MaterialTheme.colorScheme.primary.darker(0.9f),
@@ -145,59 +158,38 @@ fun AddToPlayListContent(
                     containerColor = MaterialTheme.colorScheme.primary.darker(0.75f)
                 )
             }
-
             BlurredImageHeader(
-                imageUrl = track.album.imageUrl,
-                alpha = lerpScrollOffset(scrollState, 0f, 200f, reverse = true),
-                edgeTreatment = BlurredEdgeTreatment.Unbounded
+                uiState = uiState,
+                firstVisibleItemIndex = firstVisibleItemIndex,
+                firstVisibleItemScrollOffset = firstVisibleItemScrollOffset,
+                dominantColorState = dominantColorState
             )
-
             LazyColumn(
                 state = lazyListState,
                 verticalArrangement = Arrangement.spacedBy(8.dp),
-                contentPadding = PaddingValues(
-                    end = 16.dp,
-                    start = 16.dp
-                )
+                modifier = Modifier.fillMaxHeight()
             ) {
-                stickyHeader {
-                    TopAppBar(
-                        onBackPress = onBackPress,
-                        dividerAlpha = alpha,
-                        modifier = Modifier.background(
-                            MaterialTheme.colorScheme.primary
-                                .darker(0.9f)
-                                .copy(alpha = alpha)
-                            )
-                    )
-                }
+                stickyHeader { TopAppBar(onBackPress = onBackPress, alpha = alpha) }
                 item {
                     Sort(
                         sortOption = currentSortOption,
                         modifier = Modifier
-                            .padding(vertical = 16.dp)
-                            .clickable {
-                                showBottomSheet = true
-                            }
+                            .padding(16.dp)
+                            .clickable { showBottomSheet = true }
                     )
                 }
-                playlistsList(playlists = playlists, onPlaylistClick = {}, onNavigateToPlaylistClick = {})
-                item {
-                    Spacer(modifier = Modifier.height(200.dp))
-                }
+                playlistsList(uiState = uiState, playlists = playlists)
+                item { Spacer(modifier = Modifier.height(200.dp)) }
             }
-
             Box(
                 modifier = Modifier
                     .align(Alignment.BottomCenter)
                     .fillMaxWidth()
                     .height(200.dp)
-                    .linearGradientScrim(
-                        color = Color.Black
-                    )
+                    .linearGradientScrim(color = Color.Black)
             ) {
                 OutlinedButton(
-                    onClick = { /*TODO*/ },
+                    onClick = { onBackPress() },
                     modifier = Modifier
                         .align(Alignment.BottomCenter)
                         .padding(32.dp)
@@ -210,13 +202,87 @@ fun AddToPlayListContent(
 }
 
 @Composable
+private fun BlurredImageHeader(
+    uiState: AddToPlaylistUiState,
+    firstVisibleItemIndex: Int,
+    firstVisibleItemScrollOffset: Int,
+    dominantColorState: DominantColorState,
+    modifier: Modifier = Modifier
+) {
+    when (uiState) {
+        AddToPlaylistUiState.Loading -> Unit
+        is AddToPlaylistUiState.Success -> {
+
+            // When the selected image url changes, call updateColorsFromImageUrl() or reset()
+            LaunchedEffect(uiState.track.album.imageUrl) {
+                dominantColorState.updateColorsFromImageUrl(uiState.track.album.imageUrl)
+            }
+
+            BlurredImageHeader(
+                imageUrl = uiState.track.album.imageUrl,
+                alpha = if (firstVisibleItemIndex == 0) lerpScrollOffset(
+                    scrollOffset = abs(firstVisibleItemScrollOffset),
+                    valueMin = 0f,
+                    valueMax = 200f,
+                    reverse = true) - 0.2f else 0f,
+                edgeTreatment = BlurredEdgeTreatment.Unbounded,
+                modifier = modifier
+            )
+        }
+    }
+}
+
+private fun LazyListScope.playlistsList(
+    uiState: AddToPlaylistUiState,
+    playlists: LazyPagingItems<SimplifiedPlaylist>,
+) {
+    when (uiState) {
+        AddToPlaylistUiState.Loading -> {
+            items(count = 5) {
+                AnimationBox {
+                    RectangleRoundedCornerPlaceholder(
+                        width = dimensionResource(id = R.dimen.player_card_width),
+                        height = 70.dp,
+                        cornerSize = 12.dp,
+                        modifier = Modifier
+                            .padding(horizontal = 16.dp)
+                            .testTag("addToPlaylist:playlistsLoading")
+                    )
+                }
+            }
+        }
+        is AddToPlaylistUiState.Success -> {
+            items(
+                items = playlists.itemSnapshotList,
+                key = { it?.id ?: "0" }
+            ) { playlist ->
+                // rememberSaveable is needed in order to save selection state during scrolling
+                var isSelected by rememberSaveable { mutableStateOf(false) }
+                AnimationBox {
+                    PlaylistCard(
+                        name = playlist!!.name,
+                        ownerName = playlist.ownerName,
+                        imageUrl = playlist.imageUrl,
+                        isSelected = isSelected,
+                        isSelectable = true,
+                        onClick = { isSelected = !isSelected },
+                        modifier = Modifier.padding(horizontal = 16.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
 private fun TopAppBar(
     onBackPress: () -> Unit,
     modifier: Modifier = Modifier,
-    dividerAlpha: Float = 1f
+    alpha: Float = 1f
 ) {
     Column(modifier = modifier
         .fillMaxWidth()
+        .background(Color.Black.copy(alpha = alpha))
     ) {
         Spacer(Modifier.windowInsetsTopHeight(WindowInsets.safeDrawing))
         Row(
@@ -241,14 +307,12 @@ private fun TopAppBar(
                     style = MaterialTheme.typography.headlineSmall,
                     modifier = Modifier
                         .wrapContentSize()
-                        .constrainAs(text) {
-                            centerHorizontallyTo(parent)
-                        }
+                        .constrainAs(text) { centerHorizontallyTo(parent) }
                 )
+            }
         }
+        HorizontalDivider(modifier = Modifier.alpha(alpha))
     }
-        HorizontalDivider(modifier = Modifier.alpha(dividerAlpha))
-}
 }
 
 @Preview
@@ -256,8 +320,22 @@ private fun TopAppBar(
 fun AddToPlayListPreview() {
     MyMusicTheme {
         AddToPlayListContent(
-            track = PreviewParameterData.tracks[0],
+            uiState = AddToPlaylistUiState.Success(PreviewParameterData.tracks[0]),
             playlists = flowOf(PagingData.from(PreviewParameterData.simplifiedPlaylists)).collectAsLazyPagingItems(),
+            currentSortOption = SortOption.RECENTLY_ADDED,
+            onSortOptionChanged = {},
+            onBackPress = {}
+        )
+    }
+}
+
+@Preview
+@Composable
+fun AddToPlayListLoadingPreview() {
+    MyMusicTheme {
+        AddToPlayListContent(
+            uiState = AddToPlaylistUiState.Loading,
+            playlists = flowOf(PagingData.from(emptyList<SimplifiedPlaylist>())).collectAsLazyPagingItems(),
             currentSortOption = SortOption.RECENTLY_ADDED,
             onSortOptionChanged = {},
             onBackPress = {}
