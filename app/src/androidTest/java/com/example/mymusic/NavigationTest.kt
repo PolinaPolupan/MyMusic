8821b0661b0dd024ce632ff4c57e9c37ef1af850
PolinaPolupan <polupanpolina9@gmail.com>
2024-09-01 20:32:13 +0300

    test: Add navigation tests

--- app/src/androidTest/java/com/example/mymusic/NavigationTest.kt
+++ app/src/androidTest/java/com/example/mymusic/NavigationTest.kt
@@ -1,7 +1,10 @@
 package com.example.mymusic
 
-import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Before
@@ -19,15 +22,67 @@ class NavigationTest {
 
 
     @get:Rule(order = 1)
-    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
 
     @Before
     fun setup() {
         hiltRule.inject()
     }
 
+    /**
+     * Navigation test asserts that the first destination is Home screen.
+     */
     @Test
-    fun appNavHost_verifyStartDestination() {
+    fun appNavHost_firstScreenIsHome() {
 
+        // Animations need to be paused in order to load Home screen before Log in screen
+        composeTestRule.mainClock.autoAdvance = false // Pause animations
+
+        // Advance time by a small period before Log in screen appeears
+        composeTestRule.mainClock.advanceTimeBy(100) // Advance time to load home screen
+
+        composeTestRule
+            .onNodeWithText("Listen Now")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun navigationBar_navigatesToLibrary() {
+
+        // Animations need to be paused in order to load Home screen before Log in screen
+        composeTestRule.mainClock.autoAdvance = false // Pause animations
+
+        // Advance time by a small period before Log in screen appeears
+        composeTestRule.mainClock.advanceTimeBy(100) // Advance time to load home screen
+
+        composeTestRule
+            .onNodeWithText("Library")
+            .performClick()
+
+        composeTestRule.mainClock.advanceTimeBy(100) // Advance time to load library screen
+
+        composeTestRule
+            .onNodeWithTag("library")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun navigationBar_navigatesToSearch() {
+
+        // Animations need to be paused in order to load Home screen before Log in screen
+        composeTestRule.mainClock.autoAdvance = false // Pause animations
+
+        // Advance time by a small period before Log in screen appeears
+        composeTestRule.mainClock.advanceTimeBy(100) // Advance time to load home screen
+
+        composeTestRule
+            .onNodeWithText("Search")
+            .performClick()
+
+        composeTestRule.mainClock.advanceTimeBy(100) // Advance time to load search screen
+
+        composeTestRule
+            .onNodeWithTag("search")
+            .assertIsDisplayed()
     }
 }
