07679ad81241558972f92c1c353d3e8af0011dc5
PolinaPolupan <polupanpolina9@gmail.com>
2024-09-17 21:03:10 +0300

    refactor: Move models to the separate file

--- core/auth/src/main/java/com/example/auth/AuthorizationManager.kt
+++ core/auth/src/main/java/com/example/auth/AuthorizationManager.kt
@@ -16,8 +16,6 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import net.openid.appauth.AppAuthConfiguration
 import net.openid.appauth.AuthState
@@ -36,52 +34,6 @@ import java.security.MessageDigest
 import java.security.SecureRandom
 import javax.inject.Inject
 import javax.inject.Singleton
-
-
-@Serializable
-data class SpotifyUser(
-    @SerialName("display_name")
-    val displayName: String,
-    @SerialName("external_urls")
-    val externalUrls: ExternalUrls,
-    val href: String,
-    val id: String,
-    val images: List<SpotifyImage>,
-    val type: String,
-    val uri: String,
-    val followers: Followers,
-    val country: String,
-    val product: String,
-    @SerialName("explicit_content")
-    val explicitContent: ExplicitContent,
-    val email: String,
-)
-
-@Serializable
-data class ExplicitContent(
-    @SerialName("filter_enabled")
-    val filterEnabled: Boolean,
-    @SerialName("filter_locked")
-    val filterLocked: Boolean
-)
-
-@Serializable
-data class ExternalUrls(
-    val spotify: String
-)
-
-@Serializable
-data class Followers(
-    val href: String?,
-    val total: Int
-)
-
-@Serializable
-data class SpotifyImage(
-    val url: String,
-    val height: Int?,
-    val width: Int?
-)
 
 
 @Singleton
