e47ef6a9b0b094213d71dc9568d634b350a9a504
PolinaPolupan <polupanpolina9@gmail.com>
2024-10-15 19:36:33 +0300

    refactor: Replace userPreferencesFlow data

--- core/data/src/main/java/com/example/mymusic/core/data/fake/FakeUserDataRepository.kt
+++ core/data/src/main/java/com/example/mymusic/core/data/fake/FakeUserDataRepository.kt
@@ -5,22 +5,22 @@ import com.example.mymusic.core.datastore.MyMusicPreferencesDataSource
 import com.example.mymusic.core.model.UserPreferences
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class FakeUserDataRepository @Inject constructor(
     private val dataSource: MyMusicPreferencesDataSource
 ): UserDataRepository {
 
-    override val userPreferencesFlow: Flow<UserPreferences> = flow {
-        emit(
-            UserPreferences(
-                authState = "test",
-                displayName = "test",
-                email = "test",
-                imageUrl = "test"
-            )
+    override val userPreferencesFlow: Flow<UserPreferences> = dataSource.userPreferencesFlow.map {
+        UserPreferences(
+            authState = if (it.authState.isNullOrEmpty()) "test" else it.authState, // Initialize auth state with non-empty value to prevent authorization during testing
+            displayName = it.displayName,
+            email = it.email,
+            imageUrl = it.imageUrl
         )
     }
+
     override suspend fun updateAuthState(authState: String) = dataSource.updateAuthState(authState)
 
     override suspend fun updateUserData(displayName: String, email: String, imageUrl: String) = dataSource.updateUserData(displayName, email, imageUrl)
