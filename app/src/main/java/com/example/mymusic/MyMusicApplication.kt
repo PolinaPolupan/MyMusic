f381646a22847811e696910369ee6969d075b805
PolinaPolupan <polupanpolina9@gmail.com>
2024-11-23 21:37:21 +0300

    refactor: Add custom work manager initialization

--- app/src/main/java/com/example/mymusic/MyMusicApplication.kt
+++ app/src/main/java/com/example/mymusic/MyMusicApplication.kt
@@ -1,15 +1,33 @@
 package com.example.mymusic
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import com.example.mymusic.sync.Sync
+import dagger.hilt.EntryPoint
+import dagger.hilt.EntryPoints
+import dagger.hilt.InstallIn
 import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.components.SingletonComponent
 
 @HiltAndroidApp
-class MyMusicApplication : Application() {
+class MyMusicApplication : Application(), Configuration.Provider {
+
+    @EntryPoint
+    @InstallIn(SingletonComponent::class)
+    interface HiltWorkerFactoryEntryPoint {
+        fun workerFactory(): HiltWorkerFactory
+    }
+
+    override val workManagerConfiguration: Configuration =
+        Configuration.Builder()
+            .setWorkerFactory(EntryPoints.get(this, HiltWorkerFactoryEntryPoint::class.java).workerFactory())
+            .setMinimumLoggingLevel(android.util.Log.INFO)
+            .build()
 
     override fun onCreate() {
         super.onCreate()
         // Initialize Sync; the system responsible for keeping data in the app up to date.
-        com.example.mymusic.sync.Sync.initialize(context = this)
+        Sync.initialize(context = this)
     }
 }
